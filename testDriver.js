f1b6d599a80bc46706871b7bbfe35f20f2dea9bc
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-07-17 16:21:10 -0700

    add pid check to driver check

--- testDriver.js
+++ testDriver.js
@@ -5,6 +5,9 @@ var connect = function(args, streamOpts, cb) {
   if (args.error) {
     return cb(new Error("test_error"));
   }
+  if (!args.pid) {
+    return cb(new Error('failed to get pid'));
+  }
   var term = pty.spawn('bash', [], streamOpts);
   cb(null, term);
 };
